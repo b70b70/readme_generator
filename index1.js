eede97e9a0f2661040bd41e3b00c73645af37e61
Ali <134099889+b70b70@users.noreply.github.com>
2023-10-10 20:37:50 +0100

    added more sections to generate for readme file

--- index1.js
+++ index1.js
@@ -15,12 +15,12 @@ const promptUser = () =>
     },
     {
       type: 'input',
-      name: 'overview',
+      name: 'description',
       message: 'Provide a brief overview of your project.',
     },
     {
       type: 'input',
-      name: 'prerequisites',
+      name: 'Installation',
       message: 'Name any softwares, libraries or dependencies that users need to have installed before they can use your project. ',
     },
     {
@@ -33,17 +33,35 @@ const promptUser = () =>
         name: 'license',
         message: 'Specify the license',
         choices: ['MIT', 'Apache', 'GPL', 'EPL', 'roti']
-      }
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Explain how others can contribute to your project. Include guidelines for submitting issues, feature requests, or pull requests.',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Describe how to run tests for your project and provide any relevant details about the testing process.',
+      },
+      {
+        type: 'input',
+        name: 'questions',
+        message: 'Provide information on how users or contributors can reach out with questions or feedback. You can include contact information or direct them to an issue tracker or forum.',
+      },
 
   ]);
 
   //Define a function named generateREADME that takes the answers object as a parameter and constructs a README.md file content as a string
 const generateREADME = (answers) =>
   ` ## ${answers.name} 
-  - ${answers.overview}
-  - ${answers.prerequisites} 
+  - ${answers.description}
+  - ${answers.installation} 
   - ${answers.usage} 
-  ## ${answers.license}`;
+  # ${answers.license}
+    ${answers.contributing}
+    ${answers.tests}
+    ${answers.questions}`;
 
 promptUser()
   // write the generated README content to a file named 'README.md' using the writeFileAsync function
